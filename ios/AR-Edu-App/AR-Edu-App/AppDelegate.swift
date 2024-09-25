ccd22dae0a4eee95dc994dac3e7d943e4fd76475
priyadharshan.r <priyadharshan.r@zohocorp.com>
2024-09-25 21:57:45 +0530

    initial commit: iOS setup
     iOS basic setup with a simple Home page

---
+++ ios/AR-Edu-App/AR-Edu-App/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  AR-Edu-App
+//
+//  Created by Priyadharshan Raja on 25/09/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+                
+                // Create the HomeScreenViewController and set it as the root
+                let homeViewController = HomeScreenViewController()
+                window?.rootViewController = homeViewController
+                
+                // Make the window visible
+                window?.makeKeyAndVisible()
+
+                return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
