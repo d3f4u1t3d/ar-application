a47be71958c1ad41d186d4931ededfb914d1dc22
nachiappan <naachiappan15@gmail.com>
2024-09-27 19:33:35 +0530

    upload page

--- web/folder_structure.md
+++ web/folder_structure.md
@@ -8,3 +8,24 @@ src
         page_name.scss
 App.js -- all pages with routes
 index.js -- global router
+
+{
+    authorId:001,
+    imageSet:[
+        {
+            id:1,
+            markerImg:image Data,
+            modelImg:image Data
+        },
+        {
+            id:2,
+            markerImg:image Data,
+            modelImg:image Data
+        },
+        {
+            id:3,
+            markerImg:image Data,
+            modelImg:image Data
+        }
+    ]
+}
---
+++ web/src/components/FileUpload/FileUpload.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './fileUpload.scss'
+import { MdOutlineFileUpload } from "react-icons/md";
+import { FaImage } from "react-icons/fa";
+
+const FileUpload = ({placeholder,val,onChangeHandler ,index , name}) => {
+
+  
+  
+  return (
+    <>
+    <label className='file_upload_label' htmlFor={`file_upload${name}_${index}`}>
+      {
+        val == null ?
+        <>
+        <MdOutlineFileUpload className='upload_icon_pending'/>
+        <p className='label_text'>
+          {placeholder}
+        </p>
+        </>
+        :
+        <>
+        <FaImage className='upload_icon_success'/>
+        <p className='label_text'>
+          {val.name}
+        </p>
+        </>
+      }
+    </label>
+    <input type='file' id={`file_upload${name}_${index}`}  className='file_upload' onChange={(e)=>onChangeHandler(e,index,name)} accept="image/*" />
+    </>
+  )
+}
+
+export default FileUpload
