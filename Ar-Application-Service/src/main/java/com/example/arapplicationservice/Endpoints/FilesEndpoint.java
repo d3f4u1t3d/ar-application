496a18ab7446f921e7d3815a30b910165cd2dae4
nilashs <nilashsankar2312@gmail.com>
2024-09-29 21:20:51 +0530

    .

--- Ar-Application-Service/src/main/java/com/example/arapplicationservice/Endpoints/FilesEndpoint.java
+++ Ar-Application-Service/src/main/java/com/example/arapplicationservice/Endpoints/FilesEndpoint.java
@@ -3,20 +3,11 @@ package com.example.arapplicationservice.Endpoints;
 import com.example.arapplicationservice.Service.FilesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.core.io.ClassPathResource;
-
-import javax.sql.DataSource;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 public class FilesEndpoint {
