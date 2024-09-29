27b2fe320e697710de86561e2562b1a8ae28024b
nilashs <nilashsankar2312@gmail.com>
2024-09-29 20:06:01 +0530

    added table(domain),Repo mapping and added jdbc, jpa connection

--- Ar-Application-Service/src/main/java/com/example/arapplicationservice/Endpoints/FilesEndpoint.java
+++ Ar-Application-Service/src/main/java/com/example/arapplicationservice/Endpoints/FilesEndpoint.java
@@ -1,30 +1,45 @@
 package com.example.arapplicationservice.Endpoints;
 
-import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 public class FilesEndpoint {
+    @Autowired
+    private DataSource dataSource;
     @GetMapping("/files/{filename}")
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) throws MalformedURLException {
-        Path file = Paths.get("C:/Users/gs1-nilashs/Desktop/project Files/hackaton/ar-application/Ar-Application-Service/src/main/resources").resolve(filename);
-        Resource resource = new UrlResource(file.toUri());
-        if (resource.exists() || resource.isReadable()) {
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
-                    .body(resource);
-        } else {
-            throw new RuntimeException("File not found");
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        try{
+        Resource resource = new ClassPathResource(filename);
+        if (!resource.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
+
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+    }
+
+
 }
