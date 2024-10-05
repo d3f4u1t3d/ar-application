752b6e0f9180429c43e8d5cd6e1601bfbca91876
nilashs <nilashsankar2312@gmail.com>
2024-10-05 22:36:28 +0530

    changes sql db

--- Ar-Application-Service/src/main/resources/arQuery.sql
+++ Ar-Application-Service/src/main/resources/arQuery.sql
@@ -1,13 +1,13 @@
 create table room(
 id serial primary key,
 room_key varchar(100) not null
-  );
+);
 
 create table file_path(
 id serial primary key,
 reference_room_key int not null,
-marker_file_path varchar not null unique,
-model_file_path varchar not null unique,
+marker_file_path varchar not null,
+model_file_path varchar not null,
 constraint fk_room_room_key foreign key(reference_room_key) references room(id)
 );
 
